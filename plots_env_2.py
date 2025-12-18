df26841e148f5853359a4f2909f5f4b1fca42a21
dimitriasilveria <dimitriasilveria.ds@gmail.com>
2025-12-17 19:49:43 -0500

    plotting

--- plots_env_2.py
+++ plots_env_2.py
@@ -1,5 +1,5 @@
 import matplotlib as mpl
-mpl.use("Agg")
+# mpl.use("Agg")
 import matplotlib.pyplot as plt
 import glob
 from icecream import ic
@@ -8,16 +8,16 @@ import numpy as np
 import statistics
 import os
 import re
-
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 plt.rcParams["text.usetex"] = True
 mpl.rcParams["font.size"] = 14
-# plotting rrt results
+#plotting est results
 figures_dir = 'figures'
 os.makedirs(figures_dir, exist_ok=True)
 
+#rrt
 algorithm = 'rrt'
-rrt_folder = '/home/dimitria/PhD_codes/quadcopter_load/RRT_paths_2D_obstacle_5/*.yaml'
+rrt_folder = '/home/dimitria/PhD_codes/quadcopter_load/RRT_paths_2D_obstacle_1/*.yaml'
 rrt_files = glob.glob(rrt_folder)
 
 path_length_list = []
@@ -30,11 +30,11 @@ for file in rrt_files:
         data = yaml.safe_load(f)
     if 'path_length' in data:
         path_length_list.append(float(data['path_length']))
+        num_iteratios_list.append(data['num_iterations'])
         num_success += 1
         for command in data['commands']:
             tau_list.append(command[0])
             f_list.append(command[1])
-            num_iteratios_list.append(data['num_iterations'])
 
 min_tau = min(tau_list)
 max_tau = max(tau_list)
@@ -49,19 +49,17 @@ plt.hist(tau_list, bins=10)
 # plt.title("Histogram of $\tau$")
 plt.ylabel("Frequency")
 plt.xlabel(r"$\tau$")
-plt.savefig(f"{figures_dir}/{algorithm}_tau_histogram.pdf", bbox_inches="tight")
+plt.savefig(f"{figures_dir}/{algorithm}_tau_histogram_env_2.pdf", bbox_inches="tight")
 plt.close()
 #plotting force
 plt.hist(f_list, bins=10)
 # plt.title("Histogram of $\tau$")
 plt.ylabel("Frequency")
 plt.xlabel(r"$f$")
-plt.savefig(f"{figures_dir}/{algorithm}_force_histogram.pdf", bbox_inches="tight")
+plt.savefig(f"{figures_dir}/{algorithm}_force_histogram_env_2.pdf", bbox_inches="tight")
 plt.close()
-
-#rrt star
 algorithm = 'rrt_star'
-rrt_star_folder = '/home/dimitria/PhD_codes/quadcopter_load/RRT_star_paths_2D_obstacle_5/*.yaml'
+rrt_star_folder = '/home/dimitria/PhD_codes/quadcopter_load/RRT_star_paths_2D_obstacle_1/*.yaml'
 rrt_star_files = glob.glob(rrt_star_folder)
 
 path_length_list = []
@@ -69,6 +67,7 @@ num_success = 0
 tau_list = []
 f_list = []
 num_iteratios_list = []
+
 for file in rrt_star_files:
     with open(file, 'r') as f:
         data = yaml.safe_load(f)
@@ -97,33 +96,33 @@ plt.hist(tau_list, bins=10)
 # plt.title("Histogram of $\tau$")
 plt.ylabel("Frequency")
 plt.xlabel(r"$\tau$")
-plt.savefig(f"{figures_dir}/{algorithm}_tau_histogram.pdf", bbox_inches="tight")
+plt.savefig(f"{figures_dir}/{algorithm}_tau_histogram_env_2.pdf", bbox_inches="tight")
 plt.close()
 #plotting force
 plt.hist(f_list, bins=10)
 # plt.title("Histogram of $\tau$")
 plt.ylabel("Frequency")
 plt.xlabel(r"$f$")
-plt.savefig(f"{figures_dir}/{algorithm}_force_histogram.pdf", bbox_inches="tight")
+plt.savefig(f"{figures_dir}/{algorithm}_force_histogram_env_2.pdf", bbox_inches="tight")
 plt.close()
+
 #est algorithm
 algorithm = 'est'
-est_folder = "/home/dimitria/PhD_codes/quadcopter_load/info_obstacle_5_2/*.yaml"
+est_folder = "/home/dimitria/PhD_codes/quadcopter_load/est_obstacle_1/*.yaml"
 est_files = glob.glob(est_folder)
 tau_list = []
 f_list = []
 path_length_list = []
-num_iteratios_list = []
 num_success = 0
-
+num_iteratios_list = []
 for file in est_files:
     with open(file, 'r') as f:
         data = yaml.safe_load(f)
     if data['path_length'] != 'np.inf':
         path_length_list.append(float(data['path_length']))
+        num_iteratios_list.append(data['iterations'])
         tau_list += data['tau_list']
         f_list += data['f_list']
-        num_iteratios_list.append(data['iterations'])
         num_success +=1
 
 ic(f"percentage of success:", num_success/len(est_files))
@@ -136,18 +135,19 @@ ic(f"median of number of iterations to reach the goal: {median_iterations}")
 tau_list_zero = [float(x) for x in tau_list if float(x) != 0]
 f_list_zero   = [float(x) for x in f_list if float(x) != 0]
 #plotting torque
-plt.hist(tau_list_zero, bins=10)
+plt.hist(tau_list, bins=10)
 # plt.title("Histogram of $\tau$")
 plt.ylabel("Frequency")
 plt.xlabel(r"$\tau$")
-plt.savefig(f"{figures_dir}/{algorithm}_tau_histogram.pdf", bbox_inches="tight")
+plt.savefig(f"{figures_dir}/{algorithm}_tau_histogram_env_2.pdf", bbox_inches="tight")
 plt.xlim(min_tau, max_tau)
+plt.show()
 plt.close()
-
 #plotting force
-plt.hist(f_list_zero, bins=10)
+plt.hist(f_list, bins=10)
 # plt.title("Histogram of $\tau$")
 plt.ylabel("Frequency")
 plt.xlabel(r"$f$")
-plt.savefig(f"{figures_dir}/{algorithm}_force_histogram.pdf", bbox_inches="tight")
+plt.savefig(f"{figures_dir}/{algorithm}_force_histogram_env_2.pdf", bbox_inches="tight")
+
 plt.close()
